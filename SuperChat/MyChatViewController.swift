0317e926655acd23870ab580e96707cdaf0850e8
Дмитрий <Dmitry@Mac-Pro.Dlink>
2015-08-19 01:16:06 +0300

    Вывод доступных комнат

--- SuperChat/MyChatViewController.swift
+++ SuperChat/MyChatViewController.swift
@@ -6,18 +6,23 @@ import RealmSwift
 
 class MyChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var jsonDataInfo:JSON =  nil
+    var jsonDataRooms:JSON =  nil
+    var contactsDictionary = [Int: [String: String]]()
+    
     override func viewDidLoad() {
         
+        //получим из Mobile Database текущую сессию
         let curSession = Realm().objects(currSession2)[0]
-        //for i in 0...curSessions.count-1 {
-            //let Session = curSessions[0] as currSession2
-            println("\(curSession.session_id)")
-        //}
+        //создадим заголовок для .GET запросов
+        let headers = [
+            "X-Session-Id": "\(curSession.session_id)",
+            "Accept": "application/json",
+            "Content-type": "application/json"
+        ]
         
-        
-        /*
         //Узнаем кем является текущий залогиненный юзер
-        Alamofire.request(.GET, "http://localhost:8081/v1/info")
+        Alamofire.request(.GET, "http://localhost:8081/v1/info", headers: headers)
             .responseJSON { requestInfo, responseInfo, dataInfo, errorInfo in
                 if(errorInfo != nil) {
                     println("Error: \(errorInfo)")
@@ -27,12 +32,14 @@ class MyChatViewController: UIViewController, UITableViewDataSource, UITableView
                     println("Error: \(errorInfo)")
                     println("Request: \(requestInfo)")
                     println("Response: \(responseInfo)")
-                    var jsonDataInfo = JSON(dataInfo!)
+                    println("Data: \(dataInfo!)")
+                    self.jsonDataInfo = JSON(dataInfo!)
                 }
         }
-        
+    
+    
         //Получим все комнаты, доступные текущему юзеру
-        Alamofire.request(.GET, "http://localhost:8081/v1/rooms")
+        Alamofire.request(.GET, "http://localhost:8081/v1/rooms", headers: headers, encoding: .JSON)
             .responseJSON { requestRooms, responseRooms, dataRooms, errorRooms in
                 if(errorRooms != nil) {
                     println("Error: \(errorRooms)")
@@ -42,29 +49,47 @@ class MyChatViewController: UIViewController, UITableViewDataSource, UITableView
                     println("Error: \(errorRooms)")
                     println("Request: \(requestRooms)")
                     println("Response: \(responseRooms)")
-                    println("Data: \(dataRooms)")
-                    var jsonDataRooms = JSON(dataRooms!)
+                    println("Data: \(dataRooms!)")
+                    self.jsonDataRooms = JSON(dataRooms!)
                 }
         }
         
-        //Ищем в комнатах другогих юзеров, не являющихся текущим залогиненным. Таким образом получаем "список контактов".
-        */
-        
+        //Ищем в комнатах других юзеров, не являющихся текущим. Таким образом словарь "список контактов". Если ничего не находим, оборажаем информацию о необходимости поиска юзера для общения.
+        if jsonDataRooms.isEmpty && jsonDataInfo.isEmpty {
+            println("Контактов нет")
+        } else {
+            var i=0
+            for (idroom, users) in jsonDataRooms {
+                for (iduser, user) in users {
+                    if user != jsonDataInfo {
+                        var contactUser: [String: String] = [idroom: user.stringValue]
+                        contactsDictionary[i] = contactUser
+                        i += 1
+                    }
+                }
+            }
+        }
         
     }
     
     
     //Функция возвращает количество строк, которое надо выводить в таблице
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        if self.jsonDataRooms.count != 0 {
+            return self.jsonDataRooms.count
+        } else {
+            return 0
+        }
     }
     
-    //Функция возвращает ячейку таблицы для вывода
+    //Функция возвращает ячейку таблицы для вывода. Выводим в нее список контактов, полученный в запросе GET /v1/rooms.
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestSwiftCell")
         
-        cell.textLabel!.text = "Вася \(indexPath.row)"
-        cell.detailTextLabel!.text = "Hi, \(indexPath.row)"
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "ContactCell")
+        if !contactsDictionary.isEmpty {
+            cell.textLabel!.text = contactsDictionary[indexPath.row]?.values.first
+            cell.detailTextLabel!.text = "\(contactsDictionary[indexPath.row]?.keys.first)"
+        }
         
         return cell
     }
